1ecabb665c6d5aab1461a79a0d271851d4139f15
Carla Caroline <carla.caroline@mobyan.com.br>
2024-03-19 20:00:59 -0300

    nova atividade

---
+++ index.js
@@ -0,0 +1,16 @@
+const Livro= {
+    nome: 'harry potter',
+    linguagem:'portugues',
+    genero: 'fantasia'
+    
+    }
+    
+    let nome= prompt('Qual livro deseja?')
+    let linguagem = prompt ('Linguagem desejada')
+    
+    
+    
+    
+    console.log(Livro.nome)
+    console.log(Livro.linguagem)
+    console.log(Livro.genero)
